cc545c7ecebf5ff0cc6fe0ad017ab22801145513
lonnen <chris.lonnen@gmail.com>
2013-07-28 20:06:13 -0700

    refactor queue into a generator
    refactor queue connection and polling into a generator object that filters results and sleeps between queue calls, simplifying the main loop.
    
    also removes an uneccessary sys.exit() that was causing shove to bail after a single task was completed.

--- shove/shove.py
+++ shove/shove.py
@@ -13,20 +13,8 @@ log = logging.getLogger(__name__)
 process_manager = ProcessManager()
 
 
-def run(cmd):
-    "Run a single command and exit"
-    p = Process(cmd, stdout=sys.stdout, stderr=sys.stderr)
-    p.wait()
-    sys.exit(p.returncode)
-
-
-def main():
-    """
-    connect to queue
-    loop:
-        command = blocking_queue_poll()
-        execute(command)
-    """
+def blocking_queue():
+    "sets up a queue connection and returns a generator of blocking reads"
     params = pika.ConnectionParameters(
         host=os.environ.get("RABBITMQ_HOST"),
         port=int(os.environ.get("RABBITMQ_PORT")),
@@ -36,16 +24,33 @@ def main():
             os.environ.get("RABBITMQ_PASS")
         )
     )
-
     queue = os.environ.get("CAP_SHOVE_QUEUE", "default")
     channel = pika.BlockingConnection(params).channel()
-    while(True):
+    while (True):
         _, _, body = channel.basic_get(queue=queue, no_ack=True)
         if not body:
-            print "No message returned, sleeping 5 seconds."
+            print "Nothing yet, having a quick nap."
             time.sleep(5)
             continue
-        run(body)
+        yield body
+
+
+def run(cmd):
+    "Run a single command and exit"
+    p = Process(cmd, stdout=sys.stdout, stderr=sys.stderr)
+    p.wait()
+    print "finished running %s - returned %s" % (cmd, p.returncode)
+
+
+def main():
+    """
+    connect to queue
+    loop:
+        command = blocking_queue_poll()
+        execute(command)
+    """
+    for command in blocking_queue():
+        run(command)
 
 if __name__ == '__main__':
     main()
