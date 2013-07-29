bcda42242a87f302b6e1538f28dfe029823cb574
lonnen <chris.lonnen@gmail.com>
2013-07-29 11:48:32 -0700

    nits

--- shove/shove.py
+++ shove/shove.py
@@ -4,8 +4,8 @@ import sys
 import time
 
 import pika
-
 from honcho.process import Process, ProcessManager
+
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -14,7 +14,8 @@ process_manager = ProcessManager()
 
 
 def blocking_queue():
-    "sets up a queue connection and returns a generator of blocking reads"
+    """Create up a queue connection and returns a generator of blocking reads.
+    """
     params = pika.ConnectionParameters(
         host=os.environ.get("RABBITMQ_HOST"),
         port=int(os.environ.get("RABBITMQ_PORT")),
@@ -36,21 +37,16 @@ def blocking_queue():
 
 
 def run(cmd):
-    "Run a single command and exit"
+    """Run a single command and exit"""
     p = Process(cmd, stdout=sys.stdout, stderr=sys.stderr)
     p.wait()
     print "finished running %s - returned %s" % (cmd, p.returncode)
 
 
 def main():
-    """
-    connect to queue
-    loop:
-        command = blocking_queue_poll()
-        execute(command)
-    """
     for command in blocking_queue():
         run(command)
+
 
 if __name__ == '__main__':
     main()
